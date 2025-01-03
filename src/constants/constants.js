7dd72c5c1820d49e6c0fb841a34e93999eba85e1
jeel-aubergine <jeel@auberginesolutions.com>
2025-01-03 16:54:17 +0530

    updated constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 const constants = {
-    DB_USERNAME: 'iamjeel',
-    DB_PASSWORD: 'iamjeel',
-    DB_NAME: 'user_auth',
+    DB_USERNAME: <your-postgres-username>,
+    DB_PASSWORD: <your-postgres-password>,
+    DB_NAME: <your-postgres-database-name>,
     DB_HOST: 'localhost',
     DB_PORT: 5432,
     JWT_SECRET: '189de3f932b1ab41c70e79766b659e52a0db86378d5da30d1646aeb8e46ca1d644cb7d73ac6bc8c67e2b868687f2659a376fef96abbd4e7c9cf7f6a616a1a67f'
